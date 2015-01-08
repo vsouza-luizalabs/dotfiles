b977dfb454476db7fe89affd2afbbc5859336575
Vinicius Souza <vinicius.souza@luizalabs.com>
2015-01-08 15:28:27 -0200

    fix some errors, add osx in setup py

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
 import os
 import click
+import yaml
 
 def install():
+    osx()
+
+
     return True
+
+def osx():
+
+    stram = open("./osx/osx.yml", "r")
+    read = yaml.load(stram)
+    
+    try:
+    
+        print read['computer_name']
+        print read['host_name']
+    
+    except KeyError:
+        print "ERROR"
 
 def check():
     return False
+
+if __name__ == "__main__":
+    osx()
